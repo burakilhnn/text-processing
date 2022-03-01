c57669557a76dd97aa7e39e8eecdba6800665629
Burak Ilhan <58756954+burakilhnn@users.noreply.github.com>
2022-03-01 22:28:49 +0300

    env variable fixed

--- firstproject/settings.py
+++ firstproject/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9208c*=$e@wpjv&izfu9#gqq7$_ak%^oiex*6%9&gmnuute*c$'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'DEMO_TEST',
         'USER': 'postgres',
-        'PASSWORD': 'D47364637k*',
+        'PASSWORD': os.environ.get('DB-Password'),
         'HOST': 'localhost',
         'PORT': '5432'
     }
